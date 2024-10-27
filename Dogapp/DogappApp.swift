46d08293dfe325ba9fecfaceaf1383381617613f
Mertaince <mertaince22@gmail.com>
2024-10-27 14:01:00 +0300

    Initial Commit

---
+++ Dogapp/DogappApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogappApp.swift
+//  Dogapp
+//
+//  Created by Mert ATK on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DogappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
